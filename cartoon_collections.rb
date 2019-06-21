502d40efe9f6b77943ccf2a49feb5707df3d1024
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-21 04:05:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(veggies)
   veggies.collect do |veg|
     array << veg.capitalize
   end
-  array.join(" !")
+  array.join(" /!")
 end
 
 def long_planeteer_calls# code an argument here
