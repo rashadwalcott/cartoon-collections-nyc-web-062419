3e51679295ed2f159db6c0cba4b5a005ca722ad2
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-21 03:50:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   array =[]
   dwarves.each_with_index do |dwarf,index|
-    array << "#{index}. #{dwarf[index]}"
+    array << "#{index}. #{dwarves[index]}"
   end
 puts  array
 end
