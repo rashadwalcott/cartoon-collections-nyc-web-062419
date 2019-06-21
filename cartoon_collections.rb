9d78c3b99b05657e6feb3e8eba298b6779c60fc2
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-21 05:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(cheddar_cheese)
     array << cheese == cheese_types
     
     cheddar_cheese.find do |cheese|
-      cheese.any?
+      cheese.any?(cheddar_cheese)
 end
 puts array
 end
