3b7ea866980e597f2da315460655b16f2c1dc7dd
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-21 04:48:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long)
-  calls.long.any? 
+  calls.long.any? do |calls|
+    
 end
 
 def find_the_cheese# code an argument here
