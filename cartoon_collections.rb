2c502de224e3d524ca1893f2b655b7c2529f562f
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-21 05:34:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,7 @@ end
 def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]  
+  
    cheddar_cheese.find do |cheese|
    cheese == cheese_types
 end
